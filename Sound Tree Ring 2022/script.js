61afd3f503ff6abccb5f3a0262f7fc5ffc769e83
long-hoang <51522273+long-hoang@users.noreply.github.com>
2022-03-29 02:21:16 -0700

    Add files via upload
    - Added previous and next button
    - Added dropdown menu as library select
    - Added Audio File name on bottom 
    - Reorganize controls layout
    - Remove 'Clear' Button
    - Update button controls behaviors
    - Drawing stops at end of track
    - Removed canvas drawing limit of track, will continue to animate until stopped or track ends
    - Bug Identified: After switching tracks, large "spike" sound at start of next track, results in unintentional spike

--- Sound Tree Ring 2022/script.js
+++ Sound Tree Ring 2022/script.js
@@ -1,4 +1,7 @@
-/* This version is to be run locally for testing and development */ 
+
+const trackLibrary = document.getElementById('trackLibrary'); // drop-down selector
+var selectedTrack ;
+updateTrackTitle();
 
 
 // Canvas API Settings & Parameters
@@ -39,7 +42,7 @@ let lightness = 50;
 // Web Audio API Settings & Parameters
 
 let audio1 = new Audio();
-audio1.src = 'SR001F_2.wav'; // name of the audio file (locally)
+audio1.src = selectedTrack; // LOAD SOUND FILE
 
 let isPlaying = false;
 
@@ -47,7 +50,6 @@ let audioContext = new (window.AudioContext || window.webkitAudioContext)();
 let audioSource = audioContext.createMediaElementSource(audio1);
 let analyser = audioContext.createAnalyser();
 let volume = audioContext.createGain();
-
 
 
 
@@ -64,9 +66,8 @@ let bufferLength = analyser.fftSize;
 
 
 let dataArray = new Uint8Array(bufferLength);
- 
 
-analyser.getByteTimeDomainData(dataArray);  
+
 
 
 
@@ -84,46 +85,74 @@ function draw(){
     
 }
 
+// Track Library Selection 
+
+trackLibrary.addEventListener("change",function(){
+    resetAll();
+    selectedTrack = trackLibrary.options[trackLibrary.selectedIndex].value; // get selected value
+    audio1.src = selectedTrack; // set new track
+    document.getElementById("trackTitle").innerHTML = selectedTrack;
+    prevNextButtonDisableCheck();
+
+});
+
+// Previous & Next Buttons
+
+const buttonPrev = document.getElementById('buttonPrev');
+
+buttonPrev.addEventListener('click',function(){
+    resetAll();
+    audio1.src = trackLibrary.options[trackLibrary.selectedIndex-1].value;
+    trackLibrary.selectedIndex = trackLibrary.selectedIndex-1;
+    prevNextButtonDisableCheck();
+    updateTrackTitle();
+});
+
+
+const buttonNext = document.getElementById('buttonNext');
+
+buttonNext.addEventListener('click',function(){
+    resetAll();
+    audio1.src = trackLibrary.options[trackLibrary.selectedIndex+1].value;
+    trackLibrary.selectedIndex = trackLibrary.selectedIndex+1;
+    prevNextButtonDisableCheck();
+    updateTrackTitle();
+});
+
+prevNextButtonDisableCheck(); // for first instance 
+
 // Play Button
 const buttonPlay = document.getElementById('buttonPlay');
 
 buttonPlay.addEventListener('click',function(){
+    analyser.getByteTimeDomainData(dataArray);  
     audioContext.resume();
     isPlaying = true;
     audio1.play();
     animate();
     buttonPlay.disabled = true;
-   
-    
+    buttonSusRes.disabled = false;
 });
 
-
-// Clear Button
-const buttonClear = document.getElementById('buttonClear');
-
-buttonClear.addEventListener('click',function(){
-
-    clearCanvas();
-    
-
-});
 
 
 
 
 // Suspend/Resume Button
 const buttonSusRes = document.getElementById('buttonSusRes');
-
+buttonSusRes.disabled = true;
 buttonSusRes.addEventListener('click',function(){
     if(audioContext.state === 'running') {
         isPlaying = false;
         audio1.pause(); 
+        
         audioContext.suspend().then(function() {      
             buttonSusRes.textContent = 'Resume';
         });
       } else if(audioContext.state === 'suspended') {
         isPlaying = true;
         audio1.play();
+        
         audioContext.resume().then(function() {
             buttonSusRes.textContent = 'Pause';
         });
@@ -137,14 +166,7 @@ buttonSusRes.addEventListener('click',function(){
 const buttonReset = document.getElementById('buttonReset');
 
 buttonReset.addEventListener('click', function(){
-    isPlaying = false; 
-    audioContext.suspend();
-    audio1.pause();
-    audio1.load();
-    clearCanvas();
-    buttonPlay.disabled = false;
-    buttonSusRes.textContent = 'Pause';
-    resetCanvas();
+    resetAll();
 });
 
 
@@ -154,7 +176,7 @@ function animate(){ // animate
     
     analyser.getByteTimeDomainData(dataArray);  
     
-    if((pointEnd.x < canvas.width)&&(isPlaying === true)){
+    if((isPlaying === true)&&(audio1.ended===false)){
 
         for (let i = 0; i<bufferLength; i++){
             draw();
@@ -170,18 +192,20 @@ function animate(){ // animate
             pointEnd.y = (radius+offset)*Math.cos(angle) + canvas.height/2;
         }
 
+    } else if (audio1.ended===true){
+        buttonSusRes.disabled = true;
     } else {
         return;
     }
-      
+
     requestAnimationFrame(animate);
 }
 
-function clearCanvas(){    // clear canvas
+function clearCanvas(){    // clear canvas drawings
     ctx.clearRect(0,0,canvas.width,canvas.height);
 }
 
-function resetCanvas(){
+function resetDrawPoint(){ // reset drawing to center of canvas
     angle = 0;
     radius = 0;
 
@@ -192,3 +216,42 @@ function resetCanvas(){
     pointEnd.y = canvas.height/2;
 }
 
+function resetAll(){ // clears canvas drawing, reset track, buttons set to default
+    isPlaying = false; 
+    audioContext.suspend();
+    audio1.pause();
+    
+    clearCanvas();
+    buttonPlay.disabled = false;
+    buttonSusRes.disabled = false;
+    
+    buttonSusRes.textContent = 'Pause';
+    buttonSusRes.disabled = true;
+    resetDrawPoint();
+    audio1.load();
+    
+}
+
+function prevNextButtonDisableCheck(){ // check if the track if is at edges, if true, disable next or prev buttons accordingly
+    
+    if(trackLibrary.selectedIndex === 0){
+        buttonPrev.disabled = true;
+    }else if(trackLibrary.selectedIndex !== 0){
+        buttonPrev.disabled = false;
+    }
+    
+    
+    
+    if(trackLibrary.selectedIndex !== trackLibrary.options.length-1){
+        buttonNext.disabled = false;
+    }else if(trackLibrary.selectedIndex === trackLibrary.options.length-1){
+        buttonNext.disabled = true;
+    }
+}
+
+function updateTrackTitle(){
+    selectedTrack = trackLibrary.options[trackLibrary.selectedIndex].value; // get selected value
+
+    document.getElementById("trackTitle").innerHTML = selectedTrack; // update title based on track 
+
+}
